25c491a51e76161110f79cde9189f4d1b3b1914b
Cristhian Alexander  Forero <cristhian.forero.dominguez@gmail.com>
2019-12-19 21:17:38 -0500

    Add compuest functions

--- functional-programming/js/main.js
+++ functional-programming/js/main.js
@@ -1,28 +1,73 @@
 // const compose = (...functions) => data =>
 //     functions.reduceRight((value, func) => func(value), data);
 
-let description = $('#description');
-let carbs = $('#carbs');
-let calories = $('#calories');
-let protein = $('#protein');
+let description = document.querySelector('#description');
+let carbs = document.querySelector('#carbs');
+let calories = document.querySelector('#calories');
+let protein = document.querySelector('#protein');
 
-// Inputs Validate
-const btnAdd = $('#btn-add');
+const attrsToString = (obj = {}) => {
+  const keys = Object.keys(obj);
+  const attrs = [];
 
-description.keypress(() => description.removeClass('is-invalid'));
-carbs.keypress(() => carbs.removeClass('is-invalid'));
-calories.keypress(() => calories.removeClass('is-invalid'));
-protein.keypress(() => protein.removeClass('is-invalid'));
+  keys.forEach(key => {
+    let attr = key;
+    attrs.push(`${attr}="${obj[attr]}"`);
+  });
 
-const validateInputs = () => {
-  description.val() ? '' : description.addClass('is-invalid');
-  carbs.val() ? '' : carbs.addClass('is-invalid');
-  calories.val() ? '' : calories.addClass('is-invalid');
-  protein.val() ? '' : protein.addClass('is-invalid');
+  return attrs.join('');
+}
 
-  if (description.val() && carbs.val() && calories.val() && protein.val()) {
-      
+const tagAttrs = obj => (content = "") => `
+  <${obj.tag}${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>
+`;
+
+const tag = t => {
+  if (typeof t === 'string') {
+    tagAttrs({tag: t});
+  }else {
+    tagAttrs(t);
   }
 };
 
-btnAdd.click(validateInputs);
+let list = [];
+
+// Inputs Validate
+const btnAdd = document.querySelector('#btn-add');
+
+description.onkeypress = () => description.classList.remove('is-invalid');
+carbs.onkeypress = () => carbs.classList.remove('is-invalid');
+calories.onkeypress = () => calories.classList.remove('is-invalid');
+protein.onkeypress = () => protein.classList.remove('is-invalid');
+
+const validateInputs = () => {
+  description.value ? '' : description.classList.add('is-invalid');
+  carbs.value ? '' : carbs.classList.add('is-invalid');
+  calories.value ? '' : calories.classList.add('is-invalid');
+  protein.value ? '' : protein.classList.add('is-invalid');
+
+  if (description.value && carbs.value && calories.value && protein.value) {
+    add();
+  }
+};
+
+const add = () => {
+  const newItem = {
+    description: description.value,
+    calories: parseInt(calories.value),
+    carbs: parseInt(carbs.value),
+    protein: parseInt(protein.value)
+  };
+
+  list.push(newItem);
+  cleanInputs();
+}
+
+const cleanInputs = () => {
+  description.value = '';
+  calories.value = '';
+  carbs.value = '';
+  protein.value = '';
+}
+
+btnAdd.addEventListener('click', validateInputs);
